e271ce381ab49ab8b2b405edf203da35cd2640a5
fadhel07 <fmapriansyah3@gmail.com>
2023-01-16 09:41:05 +0700

    update navbar

--- page/home/header.php
+++ page/home/header.php
@@ -1,6 +1,6 @@
 <div class="row mt-5">
     <div class="col-md-6">
-        <small>SELAMAT DATANG</small>
+        <h3>SELAMAT DATANG</h3>
         <h1>DI WEBSITE <span class="text-danger">TPAKD</span> KOTA TASIKMALAYA</h1>
     </div>
     <div class="col-md-6"></div>
